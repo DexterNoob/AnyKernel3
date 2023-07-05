807d956a83ba8ef63ec56c08b6482cd125156795
dexter <115158271+DexterNoob@users.noreply.github.com>
2023-07-05 07:26:50 +0700

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=kontolodon by osm0sis @ xda-developers
+kernel.string= Dext-OSx By Mr.Kintil
 do.devicecheck=1
 do.modules=0
 do.systemless=1
